0daf25670cdd5b6b3ddb00cc129deb2a214b1ceb
Jan Decker <Jan.Decker23@gmail.com>
2024-12-17 22:38:57 +0100

    Update Game.jsx

--- src/sections/Game.jsx
+++ src/sections/Game.jsx
@@ -100,7 +100,7 @@ const Game = () => {
         <AddPlayer score={score} onAddPlayer={handleAddPlayer}  />
       ) : (
         <div className="flex flex-col items-center justify-center z-20">
-          <div className="absolute bottom-4 right-4 bg-blue-950 text-yellow-500 px-4 py-2 rounded-lg shadow-lg text-lg">
+          <div className="absolute  md:bottom-4 right-4 bg-blue-950 text-yellow-500 px-4 py-2 rounded-lg shadow-lg text-lg z-40">
             Správné odpovědi: {score}
           </div>
 
